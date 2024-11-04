8c02d528ecad3f36bf6861168b285424342c1eec
LV <viveiros.lucas@hotmail.com>
2024-11-04 18:43:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,8 @@ app.post('/fetch_external_image', async (req, res) => {
   }
 })
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 
 function request(url, returnBuffer = true, timeout = 10000) {
   return new Promise(function(resolve, reject) {
